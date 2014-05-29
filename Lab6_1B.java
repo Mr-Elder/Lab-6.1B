13d2e6e46adc8b928269e6a67f5278ba6459ef37
b3zman41 <terencebezman@aol.com>
2014-05-28 21:40:49 -0400

    Init commit

---
+++ Lab6_1B.java
@@ -0,0 +1,17 @@
+/**
+ * @(#)Lab6_1B.java
+ *
+ *
+ * @author 
+ * @version 1.00 2014/5/28
+ */
+
+
+public class Lab6_1B {
+
+   public static void main(String[] args){
+   		GradeQuiz.gradeQuiz();
+   }
+    
+    
+}
